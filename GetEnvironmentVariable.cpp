bf34c65c77df925bcfa4b0b8107ceb91ce555c55
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-12-03 10:48:16 +0800

    Create GetEnvironmentVariable.cpp

---
+++ GetEnvironmentVariable.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main() {
+    WCHAR buffer[32767];
+
+    DWORD result = GetEnvironmentVariable(L"PATH", buffer, sizeof(buffer));
+
+    if (result == 0) {
+        printf("The environment variable \"PATH\" was not found.\n");
+        return 1;
+    }
+
+    wprintf(L"The value of the environment variable \"PATH\" is: %s\n", buffer);
+    // int wprintf (const wchar_t* format, ...);
+    // wprintf is a wide-character version of printf; format is a wide-character string.
+
+    return 0;
+}
