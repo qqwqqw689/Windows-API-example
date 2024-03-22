2511964a680ce78a1049d1897bd484c27347d2ca
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2024-03-22 22:09:05 +0800

    updates

--- GetForegroundWindow.cpp
+++ GetForegroundWindow.cpp
@@ -5,17 +5,34 @@ int main() {
     // Get the handle to the foreground window
     HWND foregroundWindow = GetForegroundWindow();
 
+    // HWND : A handle to a window.
+
     // Get the length of the window title
     int titleLength = GetWindowTextLengthW(foregroundWindow) + 1;
 
     // Allocate memory to store the window title
     wchar_t* title = new wchar_t[titleLength];
 
+    // wchar_t : type for wide character representation 
+
     // Get the window title
     GetWindowTextW(foregroundWindow, title, titleLength);
 
+    /*
+    int GetWindowTextW(
+      [in]  HWND   hWnd,
+      [out] LPWSTR lpString,
+      [in]  int    nMaxCount    : The maximum number of characters to copy to the buffer, including the null character. 
+    );
+    */
+
     // Print the window title
     std::wcout << L"Foreground window title: " << title << std::endl;
+
+    // L"s-char-seq (optional)" : wide string literal
+
+    // extern basic_ostream<wchar_t> wcout;
+    // Standard output stream (wide)
 
     // Free allocated memory
     delete[] title;
